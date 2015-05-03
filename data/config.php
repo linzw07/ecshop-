372ce92c8885918771d2c1bd8cb88138cef5e4f4
zengwang lin <zengwanglin@zengwangs-MacBook-Pro.local>
2015-05-03 15:53:41 +1200

    first commit

---
+++ data/config.php
@@ -0,0 +1,35 @@
+<?php
+// database host
+$db_host   = "localhost:8080";
+
+// database name
+$db_name   = "ecshop";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "Pa55word";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "America/Los_Angeles";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2015-05-02 20:43:23');
+
+?>
